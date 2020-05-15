1cc57f145c61ee04a766594e355bf4a29f6d494d
dikshita798 <61337752+dikshita798@users.noreply.github.com>
2020-05-15 20:12:15 +0530

    Add files via upload

---
+++ Working Directory/vue/mail@uto/src/components/HelloWorld.vue
@@ -0,0 +1,166 @@
+<!--
+<template>
+
+  <div>
+<p>{{posts}}</p>
+<p>{{errors}}</p>
+</div>
+  <ul>
+    <li v-for="post of posts" v-bind:key="post.title">
+      <p><strong>{{post.title}}</strong></p>
+      <p>{{post.body}}</p>
+    </li>
+
+    <li v-for="error of errors" v-bind:key="error.message">
+      {{error.message}}
+    </li>
+  </ul> 
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios.get(`http://127.0.0.1:8000`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
+}
+</script>
+-->
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Vue Axios Post - ItSolutionStuff.com</div>
+    
+                    <div class="card-body">
+                        <form @submit="formSubmit">
+                        <strong>Name:</strong>
+                        <input type="text" class="form-control" v-model="name">
+                        <strong>Description:</strong>
+                        <textarea class="form-control" v-model="description"></textarea>
+    
+                        <button class="btn btn-success">Submit</button>
+                        </form>
+                        <strong>Output:</strong>
+                        <pre>
+                        {{output}}
+                        </pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+     
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+            return {
+              name: '',
+              description: '',
+              output: ''
+            };
+        },
+        methods: {
+            formSubmit(e) {
+                e.preventDefault();
+                let currentObj = this;
+                let axiosConfig = {
+                headers: {
+                  'Content-Type': 'application/json;charset=UTF-8',
+                  //"Origin": "http://localhost:8080",
+                 }
+                };
+                this.axios.post('http://127.0.0.1:8000/', {
+                    name: this.name,
+                    description: this.description
+                },axiosConfig)
+                .then(function (response) {
+                    currentObj.output = response.data;
+                })
+                .catch(function (error) {
+                    currentObj.output = error;
+                });
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+    <!--
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+    -->
