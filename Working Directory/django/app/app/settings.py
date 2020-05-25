441212db4fc414c23abe15d4e1783054b610654a
dikshita <98dikshitajain@gmail.com>
2020-05-25 21:11:10 +0530

    changes

--- Working Directory/django/app/app/settings.py
+++ Working Directory/django/app/app/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CSV_DIR = os.path.join(BASE_DIR,"")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -126,8 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
-SETTINGS_DIR = ''
-STATIC_DIR = os.path.abspath('.')
 EMAIL_HOST_USER = 'intrnaakash@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
