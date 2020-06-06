e821c6f1ba4d16eeb15bc07abb418971dd24c2d9
aakash1234567 <aakashkhandelwal56@gmail.com>
2020-06-06 15:15:34 +0530

    "changes"

--- Working Directory/django/app/main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import clgData,locData,ElsiCollegeDtls
-
-admin.site.register(clgData)
-admin.site.register(locData)
-admin.site.register(ElsiCollegeDtls)
